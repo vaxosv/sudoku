33aee6bd8c0c2ab16f92f83e1211ce27f9575dcf
vaxosv <vaxo.sv@gmail.com>
2020-07-29 22:01:52 +0400

    remove files

--- src/app/sudoku/services/sudoku-genenrator.service.spec.ts
+++ src/app/sudoku/services/sudoku-genenrator.service.spec.ts
@@ -10,10 +10,6 @@ describe('SudokuGenenratorService', () => {
     service = TestBed.inject(SudokuGeneratorService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
   it('should call getSudoku function and return sudoku  with 9 sections', () => {
     const result = service.getSudoku(0);
     const length = result.length;
@@ -21,14 +17,13 @@ describe('SudokuGenenratorService', () => {
     expect(length).toBeGreaterThan(0);
   });
 
-  it('should be 9 uniq numbers', () => {
+  it('should be 9 uniq numbers in section', () => {
     const result = service.getSudoku(0);
     const section = result[0];
 
-    const allNumbers = section.map((item) => item.number);
-
-    const lengthOfUniqueNumbers = new Set(allNumbers).size;
+    const lengthOfUniqueNumbers = new Set(section).size;
 
     expect(lengthOfUniqueNumbers).toBeGreaterThanOrEqual(9);
   });
+
 });
